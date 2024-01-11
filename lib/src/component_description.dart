464cf0ca3ceb943e432d4341ecefee1ffcac6fb8
lesleysin <nikandsin@gmail.com>
2024-01-11 22:20:48 +0200

    w

---
+++ lib/src/component_description.dart
@@ -0,0 +1,20 @@
+final class DuitComponentDescription {
+  final String tag;
+  final Map<String, dynamic> data;
+
+  DuitComponentDescription({
+    required this.tag,
+    required this.data,
+  });
+
+  factory DuitComponentDescription.fromJson(Map<String, dynamic> json) {
+    assert(json['tag'] != null,
+        "Tag must be provided in component description");
+    assert(json['layoutRoot'] != null,
+        "Layout must be provided in component description");
+    return DuitComponentDescription(
+      tag: json['tag'],
+      data: json['layoutRoot'] ?? {},
+    );
+  }
+}
