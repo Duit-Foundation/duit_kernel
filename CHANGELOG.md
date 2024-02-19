3054ea7f65e8dffaa871670a41074664a7e783da
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2024-02-19 17:42:54 +0200

    patch: Collecting paths to refs in json (#5)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,30 @@
+## 1.2.2
+
+- Updated DuitComponentDescription model. Added a property containing paths to refs properties in
+  objects containing them. Added function for extracting paths from json
+
 ## 1.2.1
+
 - Added meta property to script model
+
 ## 1.2.0
+
 - Added base classes for scripting implementation
+
 ## 1.1.1
+
 - The ServerAction model has been extended with new properties to maintain local executed actions
+
 ## 1.1.0
-- Added classes for implementing Duit components, such as DuitComponentDescription and ValueReference 
+
+- Added classes for implementing Duit components, such as DuitComponentDescription and
+  ValueReference
+
 ## 1.0.1
+
 - fix: code formation
+
 ## 1.0.0
-- The release contains the main models on the basis of which the implementation of the flutter_duit library is built. Models are also used for developing third-party extensions.
+
+- The release contains the main models on the basis of which the implementation of the flutter_duit
+  library is built. Models are also used for developing third-party extensions.
--- lib/src/component_description.dart
+++ lib/src/component_description.dart
@@ -1,22 +1,35 @@
+import 'package:duit_kernel/src/extensions.dart';
+
 ///Description of the component for registering it
 ///in the [DuitRegistry] under the key corresponding to the [tag] property
 final class DuitComponentDescription {
   final String tag;
   final Map<String, dynamic> data;
+  final Iterable<String> referencePaths;
 
   DuitComponentDescription({
     required this.tag,
     required this.data,
+    required this.referencePaths,
   });
+
+  static Iterable<String> _extractRefPaths(Map<String, dynamic> json) {
+    return json
+        .flatMap(breakPointKey: "refs")
+        .keys
+        .where((key) => key.endsWith("refs"));
+  }
 
   factory DuitComponentDescription.fromJson(Map<String, dynamic> json) {
     assert(
         json['tag'] != null, "Tag must be provided in component description");
     assert(json['layoutRoot'] != null,
         "Layout must be provided in component description");
+    final root = json['layoutRoot'];
     return DuitComponentDescription(
       tag: json['tag'],
-      data: json['layoutRoot'] ?? {},
+      data: root,
+      referencePaths: _extractRefPaths(root),
     );
   }
 }
