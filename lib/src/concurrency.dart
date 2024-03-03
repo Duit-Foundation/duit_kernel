ee79bbd0e41fa4b89fda516357ef02e8b9b40648
lesleysin <nikandsin@gmail.com>
2024-03-03 09:35:18 +0200

    work

--- lib/src/concurrency.dart
+++ lib/src/concurrency.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+///An object used for basic configuration of a [WorkerPool]
 base class WorkerPoolConfiguration {
+  ///The number of workers in the pool
   final int workerCount;
 
   WorkerPoolConfiguration({
@@ -8,9 +10,15 @@ base class WorkerPoolConfiguration {
   });
 }
 
+///Base class for all Task objects executed by workers
 base class Task {
+  ///The key of the task
   final String key;
+
+  ///The payload of the task
   final dynamic payload;
+
+  ///The id of the task
   late final int _taskId;
 
   Task({
@@ -23,10 +31,17 @@ base class Task {
   int get id => _taskId;
 }
 
+///Base class for all worker pools
 abstract class WorkerPool {
-  Future<void> initWithConfiguration(WorkerPoolConfiguration policy);
+  ///Whether the pool has been initialized
+  bool initialized = false;
 
+  ///Initializes the pool with the specified configuration
+  Future<void> initWithConfiguration(WorkerPoolConfiguration configuration);
+
+  ///Performs the task
   Future<Object?> perform(Task task);
 
+  ///Disposes the workers in pool
   void dispose();
 }
