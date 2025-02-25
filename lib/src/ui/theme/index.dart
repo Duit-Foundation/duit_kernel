00d147bb995dbfe3ff607ac78b178a1387c89d7b
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-02-25 22:20:50 +0200

    Added the ability to pass functions to the theme preprocessor to create tokens for custom widgets and override the creation of tokens for existing widgets

--- lib/src/ui/theme/index.dart
+++ lib/src/ui/theme/index.dart
@@ -1,3 +1,5 @@
 export 'theme.dart';
 export 'theme_override_rule.dart';
 export 'theme_loader.dart';
+export 'preprocessor.dart';
+export "theme_token.dart";
--- lib/src/ui/theme/preprocessor.dart
+++ lib/src/ui/theme/preprocessor.dart
@@ -1,7 +1,88 @@
 import 'package:duit_kernel/src/ui/theme/index.dart';
-import 'package:duit_kernel/src/ui/theme/theme_token.dart';
+
+/// Function for custom tokenization of themes.
+typedef CustomTokenizer = ThemeToken? Function(
+  String type,
+  Map<String, dynamic> themeData,
+);
 
 final class ThemePreprocessor {
+  final CustomTokenizer? customWidgetTokenizer, overrideWidgetTokenizer;
+
+  const ThemePreprocessor({
+    this.customWidgetTokenizer,
+    this.overrideWidgetTokenizer,
+  });
+
+  ///Creates token for default cases
+  ThemeToken _createToken(
+    String widgetType,
+    Map<String, dynamic> themeData,
+  ) {
+    return switch (widgetType) {
+      "Text" => TextThemeToken(
+          themeData,
+        ),
+      "Image" => ImageThemeToken(
+          themeData,
+        ),
+      "Align" ||
+      "BackdropFilter" ||
+      "ColoredBox" ||
+      "ConstrainedBoxAttributes" ||
+      "DecoratedBox" ||
+      "Expanded" ||
+      "FittedBox" ||
+      "Row" ||
+      "Column" ||
+      "SizedBox" ||
+      "Container" ||
+      "OverflowBox" ||
+      "Padding" ||
+      "Positioned" ||
+      "Opacity" ||
+      "RotatedBox" ||
+      "Stack" ||
+      "Wrap" ||
+      "Transform" =>
+        AnimatedPropOwnerThemeToken(
+          themeData,
+          widgetType,
+        ),
+      "AnimatedOpacity" => ImplicitAnimatableThemeToken(
+          themeData,
+          widgetType,
+        ),
+      "ElevatedButton" ||
+      "Center" ||
+      "IgnorePointerAttributes" ||
+      "RepaintBoundary" ||
+      "SingleChildScrollView" =>
+        DefaultThemeToken(
+          themeData,
+          widgetType,
+        ),
+      "CheckboxAttributes" ||
+      "Switch" ||
+      "TextField" =>
+        AttendedWidgetThemeToken(
+          themeData,
+          widgetType,
+        ),
+      "RadioGroupContext" => RadioGroupContextThemeToken(
+          themeData,
+        ),
+      "Radio" => RadioThemeToken(
+          themeData,
+        ),
+      "Slider" => SliderThemeToken(
+          themeData,
+        ),
+      _ => customWidgetTokenizer?.call(widgetType, themeData) ??
+          const UnknownThemeToken(),
+    };
+  }
+
   DuitTheme tokenize(Map<String, dynamic> theme) {
     final errors = <String>{};
     final tokens = <String, ThemeToken>{};
@@ -11,76 +92,28 @@ final class ThemePreprocessor {
       final themeData = theme["data"] as Map<String, dynamic>;
       final widgetType = theme["type"];
 
-      /* Ignored widget types:
-      - AnimatedBuilder
-      - GestureDetector
-      - IntrinsicHeight
-      - LifecycleStateListener
-      - ListView
-      - Meta
-      - Subtree
-      */
-      final ThemeToken token = switch (widgetType) {
-        "Text" => TextThemeToken(
-            themeData,
-          ),
-        "Image" => ImageThemeToken(
-            themeData,
-          ),
-        "Align" ||
-        "BackdropFilter" ||
-        "ColoredBox" ||
-        "ConstrainedBoxAttributes" ||
-        "DecoratedBox" ||
-        "Expanded" ||
-        "FittedBox" ||
-        "Row" ||
-        "Column" ||
-        "SizedBox" ||
-        "Container" ||
-        "OverflowBox" ||
-        "Padding" ||
-        "Positioned" ||
-        "Opacity" ||
-        "RotatedBox" ||
-        "Stack" ||
-        "Wrap" ||
-        "Transform" =>
-          AnimatedPropOwnerThemeToken(
-            themeData,
+      ThemeToken token;
+
+      if (overrideWidgetTokenizer != null) {
+        final overridedToken = overrideWidgetTokenizer!(
+          widgetType,
+          themeData,
+        );
+
+        if (overridedToken != null) {
+          token = overridedToken;
+        } else {
+          token = _createToken(
             widgetType,
-          ),
-        "AnimatedOpacity" => ImplicitAnimatableThemeToken(
             themeData,
-            widgetType,
-          ),
-        "ElevatedButton" ||
-        "Center" ||
-        "IgnorePointerAttributes" ||
-        "RepaintBoundary" ||
-        "SingleChildScrollView" =>
-          DefaultThemeToken(
-            themeData,
-            widgetType,
-          ),
-        "CheckboxAttributes" ||
-        "Switch" ||
-        "TextField" =>
-          AttendedWidgetThemeToken(
-            themeData,
-            widgetType,
-          ),
-        "RadioGroupContext" => RadioGroupContextThemeToken(
-            themeData,
-          ),
-        "Radio" => RadioThemeToken(
-            themeData,
-          ),
-        "Slider" => SliderThemeToken(
-            themeData,
-          ),
-        _ => const UnknownThemeToken(),
-      };
+          );
+        }
+      } else {
+        token = _createToken(
+          widgetType,
+          themeData,
+        );
+      }
 
       for (var entry in themeData.entries) {
         final key = entry.key;
