93b09e8a73911e7fdff0ae4f453d135db72ccece
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-06-30 21:28:40 +0200

    clean dead code

--- test/misc.dart
+++ test/misc.dart
@@ -1,31 +1,5 @@
 import 'package:duit_kernel/duit_kernel.dart';
 
-// final class TestAttrParser implements AttributeParserBase {
-//   dynamic _parseCustom(Map<String, dynamic> data, String tag) {
-//     return const CustomAttributes();
-//   }
-
-//   @override
-//   ViewAttribute parse(
-//     String type,
-//     Map<String, dynamic>? json,
-//     String? tag, {
-//     String? id,
-//   }) {
-//     if (tag != null) {
-//       return ViewAttribute(
-//         payload: _parseCustom(json!, tag) as T,
-//         id: id ?? "",
-//       );
-//     }
-
-//     return ViewAttribute(
-//       payload: json as T,
-//       id: id ?? "",
-//     );
-//   }
-// }
-
 final class SomeWidgetThemeToken extends ThemeToken {
   const SomeWidgetThemeToken(
     Map<String, dynamic> data,
