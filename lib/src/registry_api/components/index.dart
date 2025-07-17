610404d418d77899ca7093ac977b0e14d9b5f385
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-07-17 21:37:25 +0200

    Registry API update: Added new RefWithTarget class, changed component initialization and registration functions, removed unused files, and improved export structure. Updated tests and removed deprecated topic tokens.

--- lib/src/registry_api/components/index.dart
+++ lib/src/registry_api/components/index.dart
@@ -2,3 +2,4 @@ export "component_registry.dart";
 export "component_registry_impl.dart";
 export "component_description.dart";
 export "value_reference.dart";
+export "reference_target.dart";
--- lib/src/registry_api/index.dart
+++ lib/src/registry_api/index.dart
@@ -1,5 +1,5 @@
 export 'registry.dart';
 export 'model_factory.dart';
 export 'build_factory.dart';
-export 'reference_target.dart';
-export "components/index.dart" show ComponentDescription, ValueReference;
+export "components/index.dart"
+    show ComponentDescription, ValueReference, RefWithTarget;
