998cb07da5e576c8d9b92a0c4919bd5e6393cf89
Nikita Sin <74384266+lesleysin@users.noreply.github.com>
2025-08-02 11:48:00 +0200

    breaking: Reworking the widget model API and simplifying registration of custom widgets (#35)
    * Removed unused factory_record.dart file and updated component registration logic in DuitRegistry class, removed references to model factories. Updated factory getter methods to match new changes.
    
    * Small fix for tweens parser
    
    * rm unused API part
    
    * Update lib/src/registry_api/registry.dart
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * review fixes
    
    * fix
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- lib/src/misc/index.dart
+++ lib/src/misc/index.dart
@@ -1,5 +1,4 @@
 export 'script_runner.dart';
 export 'logger/index.dart';
 export 'parser.dart';
-export 'resource_loader.dart';
 export "annotations.dart";
--- lib/src/registry_api/factory_record.dart
+++
@@ -1,7 +0,0 @@
-import 'package:duit_kernel/duit_kernel.dart';
-
-/// The [FactoryRecord] is a [Record] of factory functions that can be used to build custom components
-typedef FactoryRecord = ({
-  ModelFactory modelFactory,
-  BuildFactory buildFactory,
-});
